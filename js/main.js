0e26926cce630daaf5dd530323f2b0f911540b39
Muhammed Ali <muhamzyali@gmail.com>
2020-05-13 20:11:44 +0100

    removed isotope.js

--- js/main.js
+++ js/main.js
@@ -224,17 +224,17 @@
 
     };
       
-    // Isotope
-    let $grid = $(".grid").isotope({
-        // options
-      });
-      // filter items on button click
-      $(".filter-button-group").on("click", "button", function() {
-        let filterValue = $(this).attr("data-filter");
-        $grid.isotope({
-          filter: filterValue
-        });
-      });
+    // // Isotope
+    // let $grid = $(".grid").isotope({
+    //     // options
+    //   });
+    //   // filter items on button click
+    //   $(".filter-button-group").on("click", "button", function() {
+    //     let filterValue = $(this).attr("data-filter");
+    //     $grid.isotope({
+    //       filter: filterValue
+    //     });
+    //   });
 
 
   
